faaa193c0291696c923d7554b2dcaec51f55b9a2
Jan Ledermann <45738753+janledermann@users.noreply.github.com>
2023-11-15 22:31:33 +0100

    feature: Remove numbers from input field after reload and replace placeholder "0" in decimal output with a dash

--- script.js
+++ script.js
@@ -3,29 +3,29 @@ document.addEventListener("DOMContentLoaded", function () {
     const decimalOutput = document.getElementById("decimal-number");
     const copyButton = document.getElementById("copy-button");
 
-    binaryInput.addEventListener("input", () => {
-        decimalOutput.innerHTML = parseInt(binaryInput.value, 2) || 0;
-    });
+    binaryInput.value = '';
 
-    copyButton.addEventListener("click", () => {
+    binaryInput.addEventListener("input", updateDecimalOutput);
+
+    copyButton.addEventListener("click", copyToClipboard);
+
+    function updateDecimalOutput() {
+        decimalOutput.innerHTML = parseInt(binaryInput.value, 2) || 0;
+    }
+
+    function copyToClipboard() {
         const textToCopy = decimalOutput.innerText;
 
-        // Create a temporary textarea element to hold the text to copy
         const tempTextarea = document.createElement("textarea");
         tempTextarea.value = textToCopy;
 
-        // Append the textarea to the DOM
         document.body.appendChild(tempTextarea);
 
-        // Select the text inside the textarea
         tempTextarea.select();
-        tempTextarea.setSelectionRange(0, 99999); // For mobile devices
+        tempTextarea.setSelectionRange(0, 99999);
 
-        // Copy the selected text to the clipboard
         document.execCommand("copy");
 
-        // Remove the temporary textarea
         document.body.removeChild(tempTextarea);
-    });
-
+    }
 });
