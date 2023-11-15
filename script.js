d2d65f84bd0c89e7609c3050a237683c40d68d2e
Jan Ledermann <45738753+janledermann@users.noreply.github.com>
2023-11-16 00:29:27 +0100

    feat: Add dark mode toggle function

--- script.js
+++ script.js
@@ -1,48 +1,64 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const binaryInput = document.getElementById("binary");
-    const decimalOutput = document.getElementById("decimal-number");
-    const copyButton = document.getElementById("copy-button");
+  const binaryInput = document.getElementById("binary");
+  const decimalOutput = document.getElementById("decimal-number");
+  const copyButton = document.getElementById("copy-button");
 
-    binaryInput.value = ''; // resets input after reload
+  binaryInput.value = ""; // resets input after reload
 
-    binaryInput.addEventListener("input", updateDecimalOutput);
-    copyButton.addEventListener("click", copyToClipboard);
+  binaryInput.addEventListener("input", updateDecimalOutput);
+  copyButton.addEventListener("click", copyToClipboard);
 
-    const containsInvalidChars = arr => arr.some(bit => bit !== '0' && bit !== '1');
+  const containsInvalidChars = (arr) =>
+    arr.some((bit) => bit !== "0" && bit !== "1");
 
-    function handleInvalidInput() {
-        alert("This character is not permitted!");
-        binaryInput.value = '';
-        decimalOutput.innerText = '-';
+  function handleInvalidInput() {
+    alert("This character is not permitted!");
+    binaryInput.value = "";
+    decimalOutput.innerText = "-";
+  }
+
+  function updateDecimalOutput() {
+    const binaryInputArray = binaryInput.value.split("");
+
+    if (containsInvalidChars(binaryInputArray)) {
+      handleInvalidInput();
+      return;
     }
 
-    function updateDecimalOutput() {
-        const binaryInputArray = binaryInput.value.split('');
+    decimalOutput.innerHTML = binaryInputArray.every((bit) => bit === "0")
+      ? 0
+      : parseInt(binaryInput.value, 2);
+  }
 
-        if (containsInvalidChars(binaryInputArray)) {
-            handleInvalidInput();
-            return;
-        }
+  function copyToClipboard() {
+    const textToCopy = decimalOutput.innerText;
 
-        decimalOutput.innerHTML = binaryInputArray.every(bit => bit === '0') ? 0 : parseInt(binaryInput.value, 2);
+    if (decimalOutput.innerHTML !== "-") {
+      const tempTextarea = document.createElement("textarea");
+      tempTextarea.value = textToCopy;
+      document.body.appendChild(tempTextarea);
+
+      tempTextarea.select();
+      tempTextarea.setSelectionRange(0, 99999);
+
+      document.execCommand("copy");
+
+      document.body.removeChild(tempTextarea);
+    } else {
+      alert("Can't copy if there isn't anything to copy.");
     }
+  }
 
-    function copyToClipboard() {
-        const textToCopy = decimalOutput.innerText;
+  let toggleMode = document.querySelector("#toggle-mode");
 
-        if (decimalOutput.innerHTML !== '-') {
-            const tempTextarea = document.createElement("textarea");
-            tempTextarea.value = textToCopy;
-            document.body.appendChild(tempTextarea);
+  toggleMode.addEventListener("click", () => {
+    document.documentElement.classList.toggle("dark-mode");
+  });
 
-            tempTextarea.select();
-            tempTextarea.setSelectionRange(0, 99999);
-
-            document.execCommand("copy");
-
-            document.body.removeChild(tempTextarea);
-        } else {
-            alert("Can't copy if there isn't anything to copy.");
-        }
-    }
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  ) {
+    document.documentElement.classList.toggle("dark-mode");
+  }
 });
